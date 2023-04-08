46ccb5b564a425b265ddccf1390971dc221025c0
ilia.ozhmegov <elijahozhmegov@gmail.com>
2023-04-08 23:39:17 +0200

    add stacked

--- 502. IPO/Solution.py
+++ 502. IPO/Solution.py
@@ -1,5 +1,6 @@
 from bisect import bisect_left
 from collections import defaultdict
+from collections import Counter
 from typing import List
 import heapq
 
@@ -39,9 +40,39 @@ class CostProfit:
         return -heapq.heappop(self.profit) if len(self.profit) > 0 else -1
 
 
+class CostProfit:
+    def __init__(self, profits: List[int], capital: List[int], w: int) -> None:
+        self.profits_counter = Counter(profits)
+        self.capital_counter = Counter(capital)
+        self.cost_profit = defaultdict(int)
+
+        for profit, cost in zip(profits, capital):
+            cost_to_save = max(cost, w)
+            self.cost_profit[cost_to_save] = max(self.cost_profit[cost_to_save], profit)
+
+        self.costs = sorted(self.cost_profit.keys())
+        self.profits = sorted(self.profits_counter.keys())
+
+    def __len__(self) -> int:
+        return len(self.profits_counter)
+
+    def pop_max_profit_for(self, cost):
+        while len(self.costs) and cost >= self.costs[0]:
+            self.cost_profit[cost] = max(self.cost_profit[cost], self.cost_profit.pop(self.costs.pop(0)))
+
+        if self.cost_profit[cost] == 0: return -1
+        i = bisect_left(self.profits, self.cost_profit[cost])
+        profit = self.profits[i]
+        self.profits_counter[profit] -= 1
+        if self.profits_counter[profit] == 0:
+            self.profits_counter.pop(profit)
+            self.profits.pop(i)
+        self.cost_profit[cost+profit] = self.cost_profit[cost]
+        return profit
+
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        cost_profit = CostProfit(profits, capital, w)
+        cost_profit = CostProfit2(profits, capital, w)
 
         while k and len(cost_profit):
             profit = cost_profit.pop_max_profit_for(w)
